0086365e72abf7ec14376924e05f0b9111856803
Skipper <vasilikhinnikita@gmail.com>
2021-08-05 15:12:03 +0300

    Add Two Numbers

---
+++ Source/LeetCodeNet/AddTwoNumbers.cs
@@ -0,0 +1,51 @@
+﻿namespace LeetCodeNet.AddTwoNumbers
+{
+   /// <summary>
+   /// You are given two non-empty linked lists representing two non-negative integers.
+   /// The digits are stored in reverse order, and each of their nodes contains a single digit.
+   /// Add the two numbers and return the sum as a linked list.
+   /// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+   /// https://leetcode.com/problems/add-two-numbers/
+   /// </summary>
+   public class Solution
+   {
+      public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+      {
+         int carry = 0;
+         ListNode cur1 = l1;
+         ListNode cur2 = l2;
+
+         ListNode resRoot = new ListNode(0);
+         ListNode resCur = resRoot;
+         while (cur1 != null || cur2 != null)
+         {
+            int sum = (cur1?.val ?? 0) + (cur2?.val ?? 0) + carry;
+            carry = sum / 10;
+
+            resCur.next = new ListNode(sum % 10);
+            resCur = resCur.next;
+
+            cur1 = cur1?.next;
+            cur2 = cur2?.next;
+         }
+
+         if (carry > 0)
+            resCur.next = new ListNode(carry);
+
+         return resRoot.next;
+      }
+   }
+
+   /// <summary>Definition for singly-linked list.</summary>
+   public class ListNode
+   {
+      public int val;
+      public ListNode next;
+
+      public ListNode(int val = 0, ListNode next = null)
+      {
+         this.val = val;
+         this.next = next;
+      }
+   }
+}
---
+++ Source/LeetCodeNetTests/AddTwoNumbersTest.cs
@@ -0,0 +1,64 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeNet.AddTwoNumbers;
+
+namespace LeetCodeNetTests
+{
+   [TestClass]
+   public class AddTwoNumbersTest
+   {
+      [TestMethod]
+      public void Case1()
+         => runCase(createList(2, 4, 3), createList(5, 6, 4), createList(7, 0, 8));
+      [TestMethod]
+      public void Case2()
+         => runCase(createList(0, 1), createList(0, 1, 2), createList(0, 2, 2));
+      [TestMethod]
+      public void Case3()
+         => runCase(createList(), createList(0, 1), createList(0, 1));
+      [TestMethod]
+      public void Case4()
+         => runCase(createList(9, 9), createList(1), createList(0, 0, 1));
+
+      [TestMethod]
+      public void Case5()
+      {
+         ListNode zeroList = createList(0);
+         runCase(zeroList, zeroList, zeroList);
+      }
+
+      [TestMethod]
+      public void Case6()
+         => runCase(createList(9, 9, 9, 9, 9, 9, 9), createList(9, 9, 9, 9), createList(8, 9, 9, 9, 0, 0, 0, 1));
+
+      private ListNode createList(params int[] arr)
+      {
+         if (!arr.Any())
+            return null;
+
+         var root = new ListNode(arr.First());
+         var cur = root;
+         foreach (int n in arr.Skip(1))
+         {
+            cur.next = new ListNode(n);
+            cur = cur.next;
+         }
+         return root;
+      }
+
+      private void runCase(ListNode l1, ListNode l2, ListNode expected)
+         => checkListEq(expected, new Solution().AddTwoNumbers(l1, l2));
+
+      private void checkListEq(ListNode expected, ListNode result)
+      {
+         ListNode curExp = expected;
+         ListNode curRes = result;
+         while (curExp != null && curRes != null)
+         {
+            Assert.AreEqual(curExp?.val, curRes?.val);
+            curExp = curExp?.next;
+            curRes = curRes?.next;
+         }
+      }
+   }
+}
